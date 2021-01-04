4336e5cf43cc3509028a154723b28bc5cc1736eb
Isaac Fischer <ifischer@wikia-inc.com>
2021-01-04 14:58:29 -0600

    Update WikipediR.R
    Fandom fork description

--- R/WikipediR.R
+++ R/WikipediR.R
@@ -1,12 +1,12 @@
-#' @title A client library for MediaWiki's API
-#' @name WikipediR
-#' @description This package provides functions for accessing the MediaWiki API, either for
-#' Wikimedia projects or any other MediaWiki instance. For more information, see the
+#' @title A client library for Fandom's MediaWiki API 
+#' @name FandomR
+#' @description This package provides functions for accessing the MediaWiki API, specifically for
+#' Fandom instances. Based on WikipediR. For more information, see the
 #' \href{https://CRAN.R-project.org/package=WikipediR/vignettes/WikipediR.html}{vignette}.
 #' 
 #' @seealso The \href{https://CRAN.R-project.org/package=WikipediR/vignettes/WikipediR.html}{package vignette}.
 #' @docType package
 #' @importFrom httr GET user_agent stop_for_status
 #' @importFrom utils URLencode
-#' @aliases WikipediR WikipediR-package
+#' @aliases FandomR FandomR-package
 NULL
